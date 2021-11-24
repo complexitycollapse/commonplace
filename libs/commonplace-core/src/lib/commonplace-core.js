72e2b8bd992c4160dfc8feaeb1c2f01c757e0191
complexitycollapse <complexitycollapse@protonmail.com>
2021-11-24 17:55:40 +0000

    Core functions for creating docs and local names.

--- libs/commonplace-core/src/lib/commonplace-core.js
+++ libs/commonplace-core/src/lib/commonplace-core.js
@@ -1,5 +1,6 @@
 import { mockRepository } from "./mock-repository";
 import { addMethods, addProperties } from "./utils";
+import { doc } from "./docs";
 
 export function commonplaceCore(repository) {
   let obj = {};
@@ -8,16 +9,45 @@ export function commonplaceCore(repository) {
     repository
   });
 
-  function importContent(content) {
+  function ensureContent(content) {
     if (!repository.getContent(content)) {
-      repository.addContent(content);
+      repository.addContent("Name", content);
     }
     
     return "Name";
   }
 
+  function checkName(name, newBlobIdentifier) {
+    if (name === undefined) {
+      return repository.generateUniqueName();
+    }
+
+    let existingBinding = repository.resolveLocalName(name);
+
+    if (existingBinding && existingBinding !== newBlobIdentifier) {
+      throw "Name already in use";
+    }
+
+    return name;
+  }
+
+  function addContentWithLocalName(name, isScroll, content) {
+    let blobIdentifier = ensureContent(content);
+    name = checkName();
+    return repository.createLocalName(name, isScroll, blobIdentifier);
+  }
+
+  function importContent(name, content) {
+    return addContentWithLocalName(name, true, content);
+  }
+
+  function newDoc(name) {
+    return addContentWithLocalName(name, false, doc([], []));
+  }
+
   addMethods(obj, {
-    importContent
+    importContent,
+    newDoc
   });
 
   return obj;
--- libs/commonplace-core/src/lib/mock-repository.js
+++ libs/commonplace-core/src/lib/mock-repository.js
@@ -1,23 +1,58 @@
 import { addMethods } from "./utils";
 
 export function mockRepository() {
+  let uniqueKey = 0;
+
   let obj = {
     content: [],
     docs: [],
-    calls: []
+    calls: [],
+    names: []
   };
 
-  function addContent(header) {
-    obj.calls.push({ method: "addContent", header: header });
-    obj.content.push(header);
+  function resolveLocalNameInternal(name) {
+    return obj.names.find(b => b.name === name);
+  }
+
+  function addContent(identifier, content) {
+    obj.calls.push({ method: "addContent", identifier, content });
+    obj.content.push([identifier, content]);
   }
 
   function getContent(identifier) {
+    obj.calls.push({ method: "getContent", identifier });
     obj.content.forEach(c => {
       if (c === identifier) return c;
     });
 
     return undefined;
+  }
+
+  function createLocalName(name, scroll, blobIdentifier) {
+    obj.calls.push({ method: "createLocalName", name, scroll, blobIdentifier });
+    obj.names.push([name, scroll, blobIdentifier]);
+    return name;
+  }
+
+  function rebindLocalName(name, newBlobIdentifier) {
+    obj.calls.push({ method: "createLocalName", name, newBlobIdentifier});
+    let binding = resolveLocalNameInternal(name);
+    if (binding) {
+      binding[2] = newBlobIdentifier;
+      return name;
+    } else {
+      return undefined;
+    }
+  }
+
+  function resolveLocalName(name) {
+    obj.calls.push({ method: "resolveLocalName", name});
+    return resolveLocalNameInternal(name)?.newBlobIdentifier;
+  }
+
+  function generateUniqueName() {
+    uniqueKey += 1;
+    return "Name" + uniqueKey.toString();
   }
 
   function clearCalls() {
@@ -30,7 +65,11 @@ export function mockRepository() {
   addMethods(obj, {
     addContent,
     clearCalls,
-    getContent
+    getContent,
+    createLocalName,
+    rebindLocalName,
+    resolveLocalName,
+    generateUniqueName
   });
 
   clearCalls();
