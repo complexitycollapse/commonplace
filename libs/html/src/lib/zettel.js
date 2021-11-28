e4bdaa57eda25ec536b291d2ab5a8910bbde2782
complexitycollapse <complexitycollapse@protonmail.com>
2021-11-28 22:20:49 +0000

    Zettel now add link on endsets

--- libs/html/src/lib/zettel.js
+++ libs/html/src/lib/zettel.js
@@ -1,11 +1,17 @@
-import { addProperties } from '@commonplace/core';
+import { addProperties, endset } from '@commonplace/core';
 
 export function zettel(edit, endsets) {
   let obj = {};
 
+  function makeModifiedEndset(pair) {
+    let newEndset = endset(pair.endset.name, pair.endset.set);
+    newEndset.link = pair.link;
+    return newEndset;
+  }
+
   addProperties(obj, {
     edit,
-    endsets,
+    endsets: endsets.map(makeModifiedEndset),
   });
 
   return obj;
--- libs/html/src/lib/zettel.spec.js
+++ libs/html/src/lib/zettel.spec.js
@@ -4,10 +4,10 @@ import { span, link, endset } from '@commonplace/core';
 
 function make({ edit = span("origin", 1, 10), endsets } = []) {
   if (!endsets) {
-    let e1 = endset("name1", "set1"), e2 = endset("name2", "set2");
+    let e1 = endset("nameA", "setA"), e2 = endset("nameB", "setB");
     endsets = [
-      { endset: e1, link: link("type1", e1)},
-      { endset: e2, link: link("type2", e2)}
+      { endset: e1, link: link("typeA", e1)},
+      { endset: e2, link: link("typeB", e2)}
     ];
   }
 
@@ -16,15 +16,46 @@ function make({ edit = span("origin", 1, 10), endsets } = []) {
 
 test('edit returns the passed edit', () => {
   let s = span("a", 1, 2);
-  expect(make({edit: s}).edit).toEqual(s);
+  expect(make({ edit: s }).edit).toEqual(s);
 });
 
-test('link returns the passed link', () => {
+test('endsets returns the passed endsets', () => {
   let e1 = endset("name1", "set1"), e2 = endset("name2", "set2");
-  let endsets = [
+  let pairs = [
     { endset: e1, link: link("type1", e1)},
     { endset: e2, link: link("type2", e2)}
   ];
 
-  expect(make({endsets}).endsets).toEqual(endsets);
+  let z = make({ endsets: pairs });
+
+  expect(z.endsets[0]).toEqual(expect.objectContaining({name: e1.name, set: e1.set}))
+  expect(z.endsets[1]).toEqual(expect.objectContaining({name: e2.name, set: e2.set}))
+});
+
+test('the passed links become properties on the endsets', () => {
+  let e1 = endset("name1", "set1"), e2 = endset("name2", "set2");
+  let l1 = link("type1", e1), l2 = link("type2", e2);
+  let pairs = [
+    { endset: e1, link: l1 },
+    { endset: e2, link: l2 }
+  ];
+
+  let z = make({ endsets: pairs });
+
+  expect(z.endsets[0].link).toBe(l1);
+  expect(z.endsets[1].link).toBe(l2);
+});
+
+test('the original endsets are not modified with link properties', () => {
+  let e1 = endset("name1", "set1"), e2 = endset("name2", "set2");
+  let l1 = link("type1", e1), l2 = link("type2", e2);
+  let pairs = [
+    { endset: e1, link: l1 },
+    { endset: e2, link: l2 }
+  ];
+
+  make({ endsets: pairs });
+
+  expect(e1).not.toHaveProperty("link");
+  expect(e2).not.toHaveProperty("link");
 });
