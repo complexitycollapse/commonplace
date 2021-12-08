cf7bcd4145afc1800f534787fb7494ce66b43211
complexitycollapse <complexitycollapse@protonmail.com>
2021-12-08 22:41:35 +0000

    Fixed ZettelFragment warnings

--- libs/components/src/lib/zettel-fragment.js
+++ libs/components/src/lib/zettel-fragment.js
@@ -1,11 +1,12 @@
 import { ZettelComponent } from './zettel-component';
+import { Fragment } from 'react';
 
 export function ZettelFragment({ fragment }) {
   let childComponents = fragment.children.map(f => f?.frag
     ? (<ZettelFragment key={f.key} fragment={f}/>)
     : <ZettelComponent key={f.zettel.key} zettel={f.zettel}/>);
 
-  let Tag = fragment.link?.fragmentTag ?? "React.Fragment";
+  let Tag = fragment.link?.fragmentTag ?? Fragment;
 
   return (<Tag>{childComponents}</Tag>);
 }
