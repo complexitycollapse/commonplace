e240319e4fa1803c00911f369d9cdb384faad4da
complexitycollapse <complexitycollapse@protonmail.com>
2021-12-11 23:59:02 +0000

    StaticPartFetcher

--- libs/html/src/index.js
+++ libs/html/src/index.js
@@ -1,2 +1,3 @@
 export { ZettelSchneider } from './lib/zettel-schneider';
 export { RenderLink } from './lib/render-link';
+export { StaticPartFetcher } from './lib/static-part-fetcher';
---
+++ libs/html/src/lib/static-part-fetcher.js
@@ -0,0 +1,34 @@
+import { addMethods } from '@commonplace/core';
+
+export function StaticPartFetcher(origin) {
+  let obj = {};
+
+  async function getPart(edit) {
+    let result = await fetchPart(edit);
+    return result;
+  }
+
+  async function fetchPart(edit) {
+    let response = await fetch(origin + edit.origin);
+    if (edit.editType === "span") {
+      return await response.text();
+    } else if (edit.editType === "box") {
+      return await response.blob();
+    } else {
+      throw `StaticPartFetcher.getPart did not understand edit type ${edit.editType}`;
+    }
+  }
+
+  async function getObject(name) {
+    let response = await fetch(origin + name);
+    let object = await response.json();
+    return object;
+  }
+
+  addMethods(obj, {
+    getPart,
+    getObject
+  });
+
+  return obj;
+}
