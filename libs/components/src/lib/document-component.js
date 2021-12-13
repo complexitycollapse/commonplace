b719fee92319508f32e70ddbe045da52c6214550
complexitycollapse <complexitycollapse@protonmail.com>
2021-12-13 11:05:49 +0000

    Remove commented-out links

--- libs/components/src/lib/document-component.js
+++ libs/components/src/lib/document-component.js
@@ -46,21 +46,6 @@ export function DocumentComponent({ docName, cache, fetcher }) {
     loadDoc();
   }, []);
 
-  // Hack in some hard-coded content
-  // zettel[0].content = "This is Hypertext";
-  // zettel[1].content = "Here we have a long spans whose purpose is to show that text can be wrapped, formatted as paragraphs, and " + 
-  // "similar things. It will hopefully be long enough to wrap across several lines when it's rendered but you never know as high def" + 
-  // " screens are quite wide.";
-  // zettel[2].content = "Here is a second paragraph. This one doesn't have to be quite as long.";
-  // let titleLink = RenderLink(leafDataToLink());
-  // zettel[0].addEndset(titleLink.endsets[0], titleLink);
-  // let testLink = RenderLink(leafDataToLink({typ: "paragraph", es: [{ptr: [{typ: "span", ori: "origin", st: 100, ln: 1000}]}]}));
-  // zettel[1].addEndset(testLink.endsets[0], testLink);
-  // let italics = RenderLink(leafDataToLink({typ: "italics", es: [{ptr: [{typ: "span", ori:"origin", st: 2100, ln: 50}]}]}));
-  // zettel[2].addEndset(italics.endsets[0], italics);
-  // let bold = RenderLink(leafDataToLink({typ: "bold", es: [{ptr: [{typ: "span", ori:"origin", st: 2100, ln: 50}]}]}));
-  // zettel[2].addEndset(bold.endsets[0], bold);
-
   return (
     <div>
       <ZettelFragment key="froot" fragment={fragmentState}/>
