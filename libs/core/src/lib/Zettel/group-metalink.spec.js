e827b3bfa9dcf7dd8fc42e53a8ec2d95343070e5
complexitycollapse <complexitycollapse@protonmail.com>
2022-06-19 23:16:19 +0100

    Refactoring group tests

--- libs/core/src/lib/Zettel/group-metalink.spec.js
+++ libs/core/src/lib/Zettel/group-metalink.spec.js
@@ -1,22 +1,50 @@
 import { it, describe, expect } from '@jest/globals';
 import { EdlBuilder, EdlZettelBuilder, LinkBuilder, SpanBuilder } from '../builders';
 import { groupMetalinkType } from '../model';
-import { InlinePointer } from '../pointers';
+import { InlinePointer, LinkPointer } from '../pointers';
+
+function aSpan() { return SpanBuilder(); }
+
+function aTargetLink(spans, { endName = "grouping end", name = "target" } = {}) {
+  return LinkBuilder(undefined, [endName, spans]).withName(name);
+}
+
+function aMetalink(target) {
+  return LinkBuilder(groupMetalinkType, ["target", [target]], [undefined, [InlinePointer("grouping end")]]).withName("metalink");
+}
+
+function make(content, links) {
+  let edl = EdlBuilder();
+  content.forEach(c => edl.withClip(c));
+  links.forEach(l => edl.withLink(l));
+  let edlZ = EdlZettelBuilder(edl).build();
+  return edlZ.children[0].renderPointers.allPointers[0].groupletBuilders();
+}
 
 describe('groupletBuilders', () => {
   it ('return undefined if there are no metalinks', () => {
-    let span = SpanBuilder();
-    let edlZ = EdlZettelBuilder(EdlBuilder().withClip(span).withLink(LinkBuilder(undefined, [undefined, [span]]).withName("target"))).build();
-    expect(edlZ.children[0].renderPointers.allPointers[0].groupletBuilders()).toHaveLength(0);
+    let span = aSpan();
+    expect(make([span], [aTargetLink([span])])).toHaveLength(0);
   });
 
   it ('return a grouplet builder if there is a group metalink that matches the endset', () => {
-    let span = SpanBuilder();
-    let target = LinkBuilder(undefined, ["grouping end", [span]]).withName("target");
-    let edlZ = EdlZettelBuilder(EdlBuilder().withClip(span)
-      .withLink(target)
-      .withLink(LinkBuilder(groupMetalinkType, ["target", [target]], [undefined, [InlinePointer("grouping end")]]).withName("metalink"))).build();
+    let span = aSpan();
+    let target = aTargetLink([span]);
 
-    expect(edlZ.children[0].renderPointers.allPointers[0].groupletBuilders()).toHaveLength(1);
+    expect(make([span], [target, aMetalink(target)])).toHaveLength(1);
+  });
+
+  it ('does not return a grouplet builder if the endset is not a grouping one', () => {
+    let span = aSpan();
+    let target = aTargetLink([span], { endName: "non-grouping end"});
+    
+    expect(make([span], [target, aMetalink(target)])).toHaveLength(0);
+  });
+
+  it ('does not return a grouplet builder if the grouping metalink does not point to the link', () => {
+    let span = aSpan();
+    let target = aTargetLink([span]);
+
+    expect(make([span], [target, aMetalink(LinkPointer("not the target"))])).toHaveLength(0);
   });
 });
