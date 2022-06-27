ccd7b94b9410119cf1b953a413b85fac61f38a41
complexitycollapse <complexitycollapse@protonmail.com>
2022-06-27 20:11:05 +0100

    Delete auto-generated files

--- libs/html/src/lib/commonplace-html.spec.js
+++
@@ -1,7 +0,0 @@
-import { describe, it, expect } from '@jest/globals';
-import { CommonplaceHtml } from './commonplace-html';
-describe('CommonplaceHtml', () => {
-  it('should work', () => {
-    expect(CommonplaceHtml()).toEqual('html');
-  });
-});
