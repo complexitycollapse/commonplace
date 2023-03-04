a439ac9ec6ccb43cc8139830c194bef9cccafa0b
complexitycollapse <complexitycollapse@protonmail.com>
2023-03-04 23:24:16 +0000

    AttributeRoute

---
+++ libs/document-model/src/lib/Attributes/AttributeRoute.js
@@ -0,0 +1,9 @@
+export const AttributeRoute = {
+  immediateDirectTarget: "Immediate Direct Target",
+  immediateContentTarget: "Immediate Content Target",
+  immediateDirectType: "Immediate Direct Type",
+  immediateContentType: "Immediate Content Type",
+  inheritedNonDefault: "Inherited Non Default"
+};
+
+Object.freeze(AttributeRoute);
