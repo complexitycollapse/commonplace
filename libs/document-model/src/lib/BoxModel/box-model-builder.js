f9088e4b4a7f4a6e86e4ff0941087874ca2d6ebc
complexitycollapse <complexitycollapse@protonmail.com>
2023-03-22 23:51:06 +0000

    BoxModelBuilder - more exploratory code

--- libs/document-model/src/lib/BoxModel/box-model-builder.js
+++ libs/document-model/src/lib/BoxModel/box-model-builder.js
@@ -1,50 +1,62 @@
-// import { finalObject } from '@commonplace/utils';
-// import { compareLinkPriority } from '../link-priority';
+import { finalObject } from '@commonplace/utils';
+import { compareLinkPriority } from '../link-priority';
 
-// export function BoxModelBuilder(docModel) {
+export function BoxModelBuilder(docModel) {
 
-//   // This will return an ORDERED hierarchy of containers (ordered by appearance in the EDL).
-//   function getContainerHierarchy(zettel, start) {
-//     let childItems = [];
+  function isTrue(value) { return value && value !== "false"; }
 
-//     for (let i = start; i < zettel.length; ++i) {
+  function Box(originObject, members) {
+    return { originObject, members };
+  }
 
-//     }
-//     zettel.forEach(z => {
-//       // Get all the root sequences that begin at this zettel.
-//       let startingSequences = z.sequences.filter(s => !s.isSubordinated && s.members[0] === z);
-//       let sequenceHierarchies = startingSequences.map(s => [s, getContainerHierarchy(x.members, parentSequences.concat([x]))]);
-//       sequenceHierarchies.sort((a, b) => compareLinkPriority(a[0].definingLink, b[0].definingLink));
-//       childItems = childItems.concat(sequenceHierarchies);
+  // This will return an ORDERED hierarchy of containers (ordered by appearance in the EDL).
+  function getContainerHierarchy(zettel) {
+    let childItems = [];
+    let currentNakedItems = undefined;
+    let currentSequence = undefined;
 
-//       // If the child Edl is not part of a sequence then it is a direct container child.
-//       if (z.sequences.filter(s => ).length === 0)
-//       if (z.isEdl) {
-//         childItems.push([z, getContainerHierarchy(z.zettel)]);
-//       }
-//     });
+    // TODO: how are zettel with the box property handled? Zettel are not the same as clips.
+    zettel.forEach(z => {
+      if (!z.sequences.contains(currentSequence)) {
+        currentSequence = undefined;
 
-//     return childItems;
-//   }
+        // Begin processing the next box sequence, if any.
+        let startingSequences = z.sequences.filter(s => !s.isSubordinated && s.members[0] === z
+          && isTrue(docModel.definingLink.markup.get("box")));
+        if (startingSequences.length > 0) {
+          startingSequences.sort((a, b) => compareLinkPriority(a[0].definingLink, b[0].definingLink));
+          if (currentNakedItems) {
+            childItems.push(currentNakedItems);
+            currentNakedItems = undefined;
+          }
+          currentSequence = startingSequences[0];
+          // Sequences need to be processed recursively by a different fn that expands their non-box members
+          childItems.push(currentSequence);
+        }
 
-//   function isTrue(value) { return value && value !== "false"; }
+        // If the child is not part of the current sequence then it is a direct container child.
+        if (currentSequence === undefined) {
+          if (currentNakedItems === undefined) { currentNakedItems = []; }
+          // If it is and EDL that's not a box then it needs to be expanded
+          currentNakedItems.push(z);
+        }
+      }
 
-//   function Box(originObject, members) {
-//     return { originObject, members };
-//   }
+      if (currentNakedItems) {
+        childItems.push(currentNakedItems);
+      }
+    });
 
-//   function generateContainersFromBoxes(container) {
-//     let childContainers =
-//   }
+    return childItems;
+  }
 
-//   return finalObject({}, {
-//     build: () => {
-//       // First need to create a hierarchy of boxes. These can be sequences and
-//       // EDLs, including the root EDL. An EDL may be contained directly or be
-//       // part of a sequence. This needs to be calculated hierarchically all the
-//       // way down.
-//       let rootContainer = [docModel, getContainerHierarchy(docModel.zettel)];
-//       let rootBox = generateContainersFromBoxes(rootContainer);
-//     }
-//   });
-// }
+  return finalObject({}, {
+    build: () => {
+      // First need to create a hierarchy of boxes. These can be sequences and
+      // EDLs, including the root EDL. An EDL may be contained directly or be
+      // part of a sequence. This needs to be calculated hierarchically all the
+      // way down.
+      let rootContainer = [docModel, getContainerHierarchy(docModel.zettel)];
+    }
+  });
+}
