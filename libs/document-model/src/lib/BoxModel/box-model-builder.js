cb68a7690663176c39f868e1e0e260c50fc4f393
complexitycollapse <complexitycollapse@protonmail.com>
2023-03-31 00:12:47 +0100

    BoxModelBuilder

--- libs/document-model/src/lib/BoxModel/box-model-builder.js
+++ libs/document-model/src/lib/BoxModel/box-model-builder.js
@@ -1,62 +1,138 @@
 import { finalObject } from '@commonplace/utils';
 import { compareLinkPriority } from '../link-priority';
 
+/*
+  Rules of boxes:
+  1. Sequences and Edls will generate boxes if they have the right attribute.
+  2. Other sequences and Edls have their content flattened into the containing box.
+  3. The box hierarchy follows the sequence and Edl hierarchy, ignoring non-boxes.
+  4. If one box sequence overlaps with another box sequence, except in the relation
+     of parent to child, then the sequence that starts later will not generate any
+     boxes. If they start at the same point then the lower priority sequence will not
+     generate any boxes.
+  3. A box may contain only boxes or content. If it has a mixture, implicit boxes
+     will be generated to contain the content.
+  4. An implicit box will always be the largest possible box, bounded either by sibling
+     boxes, or the edges of the containing box, or paragraph markers. (TODO)
+  4. Implicit boxes do not respect Edl or sequence boundaries, only box and paragraph
+     boundaries.
+  5. Top-level Edls are always boxes. Flights are always boxes. This rule cannot be
+     overridden. They do not necessarily have the box attribute.
+*/
+
 export function BoxModelBuilder(docModel) {
 
-  function isTrue(value) { return value && value !== "false"; }
+  const isTrue = value => value && value !== "false";
+  const isBox = item => isTrue(item.markup.get("box")); // TODO: it this the right attribute?
 
   function Box(originObject, members) {
-    return { originObject, members };
+    return { isBox: true, originObject, members };
   }
 
-  // This will return an ORDERED hierarchy of containers (ordered by appearance in the EDL).
-  function getContainerHierarchy(zettel) {
-    let childItems = [];
-    let currentNakedItems = undefined;
+  // This will return an ORDERED hierarchy of boxes (ordered by appearance in the EDL).
+  // Edls and sequences that are not boxes will be flattened.
+  function convertToBox(originObject, objectChildren, inSequence) {
+    let childBoxes = [];
+    let currentImplicitBox = [];
     let currentSequence = undefined;
 
-    // TODO: how are zettel with the box property handled? Zettel are not the same as clips.
-    zettel.forEach(z => {
-      if (!z.sequences.contains(currentSequence)) {
+    function terminateImplicitBox() {
+      if (currentImplicitBox.length > 0) {
+        childBoxes.push(Box(undefined, currentImplicitBox));
+        currentImplicitBox = [];
+      }
+    }
+
+    function climbToTopBoxSequence(sequence, lastBox) {
+      if (isBox(sequence.definingLink)) { lastBox = sequence; }
+      if (!sequence.isSubordinated) { return lastBox; }
+      return climbToTopBoxSequence(sequence.parent, lastBox);
+    }
+
+    function gatherZettel(zettel, inSequence) {
+      // The box attribute is only valid on sequences and Edls. Everything else is ignored.
+
+      zettel.forEach(z => {
+        // Skip this zettel if it is part of a sequence we have previously processed.
+        // (This will happen if a box sequence was detected and a box formed from its
+        // members. We can then drop the remaining zettel in the sequence).
+        if (!z.isSequence && z.sequences.includes(currentSequence)) { return; }
+
         currentSequence = undefined;
 
-        // Begin processing the next box sequence, if any.
-        let startingSequences = z.sequences.filter(s => !s.isSubordinated && s.members[0] === z
-          && isTrue(docModel.definingLink.markup.get("box")));
-        if (startingSequences.length > 0) {
-          startingSequences.sort((a, b) => compareLinkPriority(a[0].definingLink, b[0].definingLink));
-          if (currentNakedItems) {
-            childItems.push(currentNakedItems);
-            currentNakedItems = undefined;
+        if (z.isSequence) {
+          if (isBox(z.definingLink)) {
+            terminateImplicitBox();
+            childBoxes.push(convertToBox(z, z.members, true));
+          } else {
+            gatherZettel(z.members, true);
           }
-          currentSequence = startingSequences[0];
-          // Sequences need to be processed recursively by a different fn that expands their non-box members
-          childItems.push(currentSequence);
+        } else {
+
+          if (!inSequence) {
+
+            // Check to see if any box sequences begin here. Get the highest in the sequence hierarchy.
+            let startingSequences = z.sequences.filter(s => s.members[0] === z)
+              .map(climbToTopBoxSequence)
+              .filter(s => s);
+
+            if (startingSequences.length > 0) {
+
+              terminateImplicitBox();
+
+              // Add the box sequence as a child box
+              startingSequences.sort((a, b) => compareLinkPriority(a[0].definingLink, b[0].definingLink));
+              currentSequence = startingSequences[0];
+              let sequenceBox = convertToBox(currentSequence, currentSequence.members, true);
+              childBoxes.push(sequenceBox);
+            }
+          }
+
+          // If we didn't find a box sequence...
+          if (currentSequence === undefined) {
+            if (z.isEdl) {
+              if (isBox(z)) {
+
+                terminateImplicitBox();
+
+                // The Edl is a box, so convert it to a child box.
+
+                childBoxes.push(convertToBox(z, z.zettel, false));
+              } else {
+                // If it's an EDL that's not a box then it needs to be flattened.
+
+                gatherZettel(z.zettel, false);
+              }
+            } else {
+              // It's just content, so add it to an implicit box.
+
+              currentImplicitBox.push(z);
+            }
+          }
         }
+      });
+    }
 
-        // If the child is not part of the current sequence then it is a direct container child.
-        if (currentSequence === undefined) {
-          if (currentNakedItems === undefined) { currentNakedItems = []; }
-          // If it is and EDL that's not a box then it needs to be expanded
-          currentNakedItems.push(z);
-        }
-      }
+    gatherZettel(objectChildren, inSequence);
 
-      if (currentNakedItems) {
-        childItems.push(currentNakedItems);
-      }
-    });
+    // Finalize any implicit boxes. An implicit box is only required if there were
+    // other boxes in the Edl.
+    if (currentImplicitBox.length > 0) {
+      if (childBoxes.length === 0) { childBoxes = currentImplicitBox; }
+      else { terminateImplicitBox(); }
+    }
 
-    return childItems;
+    return Box(originObject, childBoxes);
   }
 
   return finalObject({}, {
     build: () => {
-      // First need to create a hierarchy of boxes. These can be sequences and
-      // EDLs, including the root EDL. An EDL may be contained directly or be
+      // First need to create a hierarchy of boxes. These can be sequences or
+      // EDLs, including the root Edl. An Edl may be contained directly or be
       // part of a sequence. This needs to be calculated hierarchically all the
       // way down.
-      let rootContainer = [docModel, getContainerHierarchy(docModel.zettel)];
+      let rootBox = convertToBox(docModel, docModel.zettel, false);
+      return rootBox;
     }
   });
 }
---
+++ libs/document-model/src/lib/BoxModel/box-model-builder.spec.js
@@ -0,0 +1,312 @@
+import { describe, expect, it } from '@jest/globals';
+import { BoxModelBuilder } from './box-model-builder';
+import { DocModelBuilderBuilder, EdlBuilder, MarkupBuilder, SequenceLinkBuilder, SpanBuilder } from '../Testing/test-builders';
+
+let unique = 1;
+function aSpan() {
+  return SpanBuilder().withOrigin((unique++).toString());
+}
+
+function anEdl() {
+  return EdlBuilder(unique++);
+}
+
+function aBoxLink(target) {
+  return MarkupBuilder().endowsBoxTo(target);
+}
+
+function expectMemberMatches(box, memberIndex, clipBuilder) {
+  expect(box.members[memberIndex].pointer).toMatchObject(clipBuilder.builtObject);
+}
+
+function makeModel(clips) {
+  let builder = DocModelBuilderBuilder(EdlBuilder("document").withClips(...clips));
+  return builder;
+}
+
+function make(docModelBuilderBuilder) {
+  let docModelBuilder = docModelBuilderBuilder.build();
+  let docModel = docModelBuilder.build();
+  docModelBuilderBuilder.model = docModel;
+  let boxModelBuilder = BoxModelBuilder(docModel);
+  let boxModel = boxModelBuilder.build();
+  return boxModel;
+}
+
+describe('BoxModelBuilder', () => {
+  it('returns an empty box for an empty document', () => {
+    let box = make(makeModel([]));
+
+    expect(box.members).toEqual([]);
+  });
+
+  it('sets isBox to true on the box', () => {
+    let box = make(makeModel([]));
+
+    expect(box.isBox).toBeTruthy();
+  });
+
+  it('sets originObject to be the document model', () => {
+    let model = makeModel([]);
+
+    let box = make(model);
+
+    expect(box.originObject).toBe(model.model);
+  });
+
+  it('adds the content of the document to the box', () => {
+    let spans = [aSpan(), aSpan(), aSpan()];
+    let model = makeModel(spans);
+
+    let box = make(model);
+
+    expect(box.members).toHaveLength(3);
+    expectMemberMatches(box, 0, spans[0]);
+    expectMemberMatches(box, 1, spans[1]);
+    expectMemberMatches(box, 2, spans[2]);
+  });
+
+  it('flattens a non-box sequence into the containing box', () => {
+    let spans = [aSpan(), aSpan(), aSpan()];
+    let model = makeModel(spans).withSequenceLink(spans);
+
+    let box = make(model);
+
+    expect(box.members).toHaveLength(3);
+    expectMemberMatches(box, 0, spans[0]);
+    expectMemberMatches(box, 1, spans[1]);
+    expectMemberMatches(box, 2, spans[2]);
+  });
+
+  it('creates a box for a box sequence', () => {
+    let spans = [aSpan(), aSpan(), aSpan()];
+    let model = makeModel(spans).withBoxSequenceLink(spans);
+
+    let box = make(model);
+
+    expect(box.members).toHaveLength(1);
+    expect(box.members[0].isBox);
+    expect(box.members[0].originObject.isSequence).toBeTruthy();
+  });
+
+  it('adds the sequence content to the sequence box', () => {
+    let spans = [aSpan(), aSpan(), aSpan()];
+    let model = makeModel(spans).withBoxSequenceLink(spans);
+
+    let box = make(model).members[0];
+
+    expect(box.members).toHaveLength(3);
+    expectMemberMatches(box, 0, spans[0]);
+    expectMemberMatches(box, 1, spans[1]);
+    expectMemberMatches(box, 2, spans[2]);
+  });
+
+  it('flattens a child sequence that is not a box into the box parent', () => {
+    let spans = [aSpan(), aSpan(), aSpan()];
+    let childSequenceLinks = SequenceLinkBuilder(spans);
+    let model = makeModel(spans)
+      .withLinks(childSequenceLinks.link, childSequenceLinks.metalink)
+      .withBoxSequenceLink([childSequenceLinks.link]);
+
+    let box = make(model).members[0];
+
+    expect(box.members).toHaveLength(3);
+    expectMemberMatches(box, 0, spans[0]);
+    expectMemberMatches(box, 1, spans[1]);
+    expectMemberMatches(box, 2, spans[2]);
+  });
+
+  it('does not flatten a child sequence that is a box', () => {
+    let spans = [aSpan(), aSpan(), aSpan()];
+    let childSequenceLinks = SequenceLinkBuilder(spans);
+    let model = makeModel(spans)
+      .withLinks(childSequenceLinks.link, childSequenceLinks.metalink)
+      .withLink(aBoxLink(childSequenceLinks.link))
+      .withBoxSequenceLink([childSequenceLinks.link]);
+
+    let box = make(model).members[0];
+
+    expect(box.members).toHaveLength(1);
+    expect(box.members[0].members).toHaveLength(3);
+  });
+
+  it('creates implicit boxes around a box sequence if that content is not in an explicit box', () => {
+    let spans = [aSpan(), aSpan(), aSpan()];
+    let model = makeModel(spans).withBoxSequenceLink([spans[1]]);
+
+    let box = make(model);
+
+    expect(box.members).toHaveLength(3);
+    expect(box.members[0].originObject).toBe(undefined);
+    expectMemberMatches(box.members[0], 0, spans[0]);
+    expect(box.members[2].originObject).toBe(undefined);
+    expectMemberMatches(box.members[2], 0, spans[2]);
+  });
+
+  it('creates implicit boxes within a box sequence if it contains content and other box sequences', () => {
+    let spans = [aSpan(), aSpan(), aSpan()];
+    let childSequenceLinks = SequenceLinkBuilder([spans[1]]);
+    let model = makeModel(spans)
+      .withLinks(childSequenceLinks.link, childSequenceLinks.metalink)
+      .withLink(aBoxLink(childSequenceLinks.link))
+      .withBoxSequenceLink([spans[0], childSequenceLinks.link, spans[2]]);
+
+    let box = make(model).members[0];
+
+    expect(box.members).toHaveLength(3);
+    expect(box.members[0].originObject).toBe(undefined);
+    expectMemberMatches(box.members[0], 0, spans[0]);
+    expect(box.members[2].originObject).toBe(undefined);
+    expectMemberMatches(box.members[2], 0, spans[2]);
+  });
+
+  it('will create nested boxes for every box sequence in a box sequence', () => {
+    let spans = [aSpan(), aSpan()];
+    let childSequenceLinks1 = SequenceLinkBuilder([spans[0]]);
+    let childSequenceLinks2 = SequenceLinkBuilder([spans[1]]);
+    let model = makeModel(spans)
+      .withLinks(childSequenceLinks1.link, childSequenceLinks1.metalink)
+      .withLinks(childSequenceLinks2.link, childSequenceLinks2.metalink)
+      .withLink(aBoxLink(childSequenceLinks1.link))
+      .withLink(aBoxLink(childSequenceLinks2.link))
+      .withBoxSequenceLink([childSequenceLinks1.link, childSequenceLinks2.link]);
+
+    let box = make(model).members[0];
+
+    expect(box.members).toHaveLength(2);
+    expect(box.members[0].originObject.definingLink.pointer).toEqual(childSequenceLinks1.link.pointer);
+    expect(box.members[1].originObject.definingLink.pointer).toEqual(childSequenceLinks2.link.pointer);
+  });
+
+  it('will create unnested boxes for every box sequence in a non-box sequence', () => {
+    let spans = [aSpan(), aSpan()];
+    let childSequenceLinks1 = SequenceLinkBuilder([spans[0]]);
+    let childSequenceLinks2 = SequenceLinkBuilder([spans[1]]);
+    let model = makeModel(spans)
+      .withLinks(childSequenceLinks1.link, childSequenceLinks1.metalink)
+      .withLinks(childSequenceLinks2.link, childSequenceLinks2.metalink)
+      .withLink(aBoxLink(childSequenceLinks1.link))
+      .withLink(aBoxLink(childSequenceLinks2.link))
+      .withSequenceLink([childSequenceLinks1.link, childSequenceLinks2.link]);
+
+    let box = make(model);
+
+    expect(box.members).toHaveLength(2);
+    expect(box.members[0].originObject.definingLink.pointer).toEqual(childSequenceLinks1.link.pointer);
+    expect(box.members[1].originObject.definingLink.pointer).toEqual(childSequenceLinks2.link.pointer);
+  });
+
+  it('flattens a non-box Edl', () => {
+    let spans = [aSpan(), aSpan(), aSpan()];
+    let child = anEdl().withClips(...spans);
+    let model = makeModel([child]);
+
+    let box = make(model);
+
+    expect(box.members).toHaveLength(3);
+    expectMemberMatches(box, 0, spans[0]);
+    expectMemberMatches(box, 1, spans[1]);
+    expectMemberMatches(box, 2, spans[2]);
+  });
+
+  it('creates a box for a box Edl', () => {
+    let spans = [aSpan(), aSpan(), aSpan()];
+    let child = anEdl().withClips(...spans);
+    let model = makeModel([child]).withLink(aBoxLink(child));
+
+    let box = make(model);
+
+    expect(box.members).toHaveLength(1);
+    expectMemberMatches(box.members[0], 0, spans[0]);
+    expectMemberMatches(box.members[0], 1, spans[1]);
+    expectMemberMatches(box.members[0], 2, spans[2]);
+  });
+
+  it('creates implicit boxes around an Edl box if that content is not in an explicit box', () => {
+    let spans = [aSpan(), aSpan(), aSpan()];
+    let child = anEdl().withClips(spans[1]);
+    let model = makeModel([spans[0], child, spans[2]])
+      .withLink(aBoxLink(child));
+
+    let box = make(model);
+
+    expect(box.members).toHaveLength(3);
+    expect(box.members[0].originObject).toBe(undefined);
+    expectMemberMatches(box.members[0], 0, spans[0]);
+    expect(box.members[2].originObject).toBe(undefined);
+    expectMemberMatches(box.members[2], 0, spans[2]);
+  });
+
+  it('creates implicit boxes within a box Edl if it contains content and other box Edls', () => {
+    let spans = [aSpan(), aSpan(), aSpan()];
+    let child = anEdl().withClips(spans[1]).withName("child");
+    let edl = anEdl().withClips(spans[0], child, spans[2]).withName("parent");
+    let model = makeModel([edl])
+      .withLink(aBoxLink(edl))
+      .withLink(aBoxLink(child));
+
+    let box = make(model).members[0];
+
+    expect(box.members).toHaveLength(3);
+    expect(box.members[0].originObject).toBe(undefined);
+    expectMemberMatches(box.members[0], 0, spans[0]);
+    expect(box.members[2].originObject).toBe(undefined);
+    expectMemberMatches(box.members[2], 0, spans[2]);
+  });
+
+  it('creates nested boxes for box Edls in box sequences', () => {
+    let spans = [aSpan(), aSpan(), aSpan()];
+    let child = anEdl().withClips(...spans);
+    let model = makeModel([child])
+      .withLink(aBoxLink(child))
+      .withBoxSequenceLink([child]);
+
+    let box = make(model);
+
+    expect(box.members).toHaveLength(1);
+    expect(box.members[0].members).toHaveLength(1);
+    expect(box.members[0].members[0].originObject.isEdl).toBeTruthy();
+  });
+
+  it('creates nested boxes for box sequences in box Edls', () => {
+    let spans = [aSpan(), aSpan(), aSpan()];
+    let sequence = SequenceLinkBuilder(spans);
+    let edl = anEdl().withClips(...spans);
+    let model = makeModel([edl])
+      .withLinks(sequence.link, sequence.metalink)
+      .withLink(aBoxLink(edl))
+      .withLink(aBoxLink(sequence.link));
+
+    let box = make(model);
+
+    expect(box.members).toHaveLength(1);
+    expect(box.members[0].members).toHaveLength(1);
+    expect(box.members[0].members[0].originObject.isSequence).toBeTruthy();
+  });
+
+  it('flattens non-box Edls in box sequences', () => {
+    let spans = [aSpan(), aSpan(), aSpan()];
+    let child = anEdl().withClips(...spans);
+    let model = makeModel([child])
+      .withBoxSequenceLink([child]);
+
+    let box = make(model);
+
+    expect(box.members).toHaveLength(1);
+    expect(box.members[0].members).toHaveLength(3);
+  });
+
+  it('flattens non-box sequences in box Edls', () => {
+    let spans = [aSpan(), aSpan(), aSpan()];
+    let sequence = SequenceLinkBuilder(spans);
+    let edl = anEdl().withClips(...spans);
+    let model = makeModel([edl])
+      .withLinks(sequence.link, sequence.metalink)
+      .withLink(aBoxLink(edl));
+
+    let box = make(model);
+
+    expect(box.members).toHaveLength(1);
+    expect(box.members[0].members).toHaveLength(3);
+  });
+});
