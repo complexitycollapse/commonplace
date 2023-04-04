29fbe5a7c8e7b5b7a2b86b52aeab0db8fd9dcd75
complexitycollapse <complexitycollapse@protonmail.com>
2023-04-04 22:07:51 +0100

    SequentialPartFetcher

--- libs/html/src/index.js
+++ libs/html/src/index.js
@@ -1,2 +1,3 @@
 export { StaticPartFetcher } from './lib/static-part-fetcher';
+export { SequentialPartFetcher } from './lib/sequential-part-fetcher';
 export { CssStyle } from './lib/css-style';
---
+++ libs/html/src/lib/sequential-part-fetcher.js
@@ -0,0 +1,17 @@
+import { finalObject } from "@commonplace/utils";
+
+export function SequentialPartFetcher(...fetchers) {
+
+  async function getPart(pointer) {
+    let lastResult = [false, "No fetchers"];
+
+    for (let fetcher of fetchers) {
+      lastResult = await fetcher(pointer);
+      if (lastResult[0]) { return lastResult; }
+    }
+
+    return lastResult;
+  }
+
+  return finalObject({}, { getPart });
+}
