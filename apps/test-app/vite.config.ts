7ea466f49bc19106757d4c76e48a940b8c7a5325
complexitycollapse <complexitycollapse@protonmail.com>
2023-04-06 21:13:09 +0100

    Upgrade test-app

--- apps/test-app/vite.config.ts
+++ apps/test-app/vite.config.ts
@@ -2,22 +2,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
+
 export default defineConfig({
   cacheDir: '../../node_modules/.vite/test-app',
+
   server: {
     port: 4200,
     host: 'localhost',
   },
+
   preview: {
     port: 4300,
     host: 'localhost',
   },
+
   plugins: [
     react(),
     viteTsConfigPaths({
       root: '../../',
     }),
   ],
+
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [
@@ -26,6 +31,7 @@ export default defineConfig({
   //    }),
   //  ],
   // },
+
   test: {
     globals: true,
     cache: {
