11abecc33b53179c592dddca0ba2223674308a7d
complexitycollapse <complexitycollapse@protonmail.com>
2023-05-11 23:33:14 +0100

    SemanticClass

---
+++ libs/document-model/src/lib/DocumentModel/semantic-class.js
@@ -0,0 +1,9 @@
+import { addProperties, finalObject } from "@commonplace/utils";
+
+export default function SemantiClass(pointer, link) {
+  let obj = {};
+
+  addProperties(obj, pointer, link);
+
+  return finalObject(obj);
+}
