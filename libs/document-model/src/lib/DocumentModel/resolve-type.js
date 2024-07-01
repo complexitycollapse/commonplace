933f16c5964260d5459b8a2c0c7692f6011c307d
complexitycollapse <complexitycollapse@protonmail.com>
2024-07-01 23:29:22 +0100

    Added comments to resolveTypeAndMetalinks

--- libs/document-model/src/lib/DocumentModel/resolve-type.js
+++ libs/document-model/src/lib/DocumentModel/resolve-type.js
@@ -1,4 +1,17 @@
+/**
+ * Resolves the type and metalinks for a given type pointer.
+ * Metalinks are those links found in the ends the type link.
+ *
+ * @param {Object} typePointer - The pointer to the type. It can be undefined, a link pointer or an inline pointer.
+ * @param {Object} cache - The cache of parts.
+ * @returns {[*, Array]} - An array where the first element is the resolved type and the second element is an array of metalink pairs.
+ */
 export default function resolveTypeAndMetalinks(typePointer, cache) {
+  /**
+   * Retrieves the type denoted by the type pointer.
+   *
+   * @returns {*} - The resolved type, which can be undefined, a link or inline text.
+   */
   function getType() {
     if (typePointer === undefined) {
       return undefined;
@@ -10,6 +23,12 @@ export default function resolveTypeAndMetalinks(typePointer, cache) {
     }
   }
 
+  /**
+   * Retrieves the metalinks for a given type.
+   *
+   * @param {*} type - The type for which to retrieve metalinks.
+   * @returns {Array} - An array of metalink pairs, each containing a metalink pointer and the denoted link.
+   */
   function getMetalinks(type) {
     let metalinkPairs = [];
 
@@ -30,3 +49,4 @@ export default function resolveTypeAndMetalinks(typePointer, cache) {
   let metalinkPairs = getMetalinks(type);
   return [type, metalinkPairs];
 }
+
