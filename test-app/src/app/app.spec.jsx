98e5406b5dab09cafbbd83975462b7adda3503b5
complexitycollapse <complexitycollapse@protonmail.com>
2024-08-19 23:45:32 +0100

    Migrated test-app to vitest

--- test-app/src/app/app.spec.jsx
+++ test-app/src/app/app.spec.jsx
@@ -1,3 +1,4 @@
+import { describe, it, expect } from 'vitest';
 import { render } from '@testing-library/react';
 
 import { BrowserRouter } from 'react-router-dom';
@@ -12,14 +13,5 @@ describe('App', () => {
       </BrowserRouter>
     );
     expect(baseElement).toBeTruthy();
-  });
-
-  it('should have a greeting as the title', () => {
-    const { getByText } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
-    expect(getByText(/Welcome test-app/gi)).toBeTruthy();
   });
 });
