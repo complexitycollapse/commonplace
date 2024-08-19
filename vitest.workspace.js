6e3023f95b776c4e9bef794eb9c67bcb82faa357
complexitycollapse <complexitycollapse@protonmail.com>
2024-08-19 23:38:41 +0100

    Added vitest.workspace.js

---
+++ vitest.workspace.js
@@ -0,0 +1,10 @@
+import { defineWorkspace } from 'vitest/config'
+
+export default defineWorkspace([
+  "./ui/vite.config.js",
+  "./test-app/vite.config.js",
+  "./core/vite.config.js",
+  "./document-model/vite.config.js",
+  "./html/vite.config.js",
+  "./utils/vite.config.js"
+])
